43105cd2f4be1ac0252ce00a465bc2290b1378f4
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-12-19 14:46:22 -0500

    Corrected incorrect filename

--- test/src/schemaParameters.js
+++ test/src/schemaParameters.js
@@ -53,4 +53,57 @@ export default function (test) {
         isMatch : true,
       };
     });
+
+  test("http://www.google.com/:x/:y")
+    .this(function () {
+      let l = new URL("http://www.google.com/:x/:y", { href: "http://www.google.com/cats/dogs" });
+      return l;
+    })
+    .isDeepEqual(function () {
+      return {
+        schema: {
+          origin   : "http://www.google.com",
+          href     : "http://www.google.com/:x/:y",
+          pathname : "/:x/:y",
+          hash     : "",
+          search   : ""
+        },
+
+        location: {
+          origin   : "http://www.google.com",
+          href     : "http://www.google.com/cats/dogs",
+          pathname : "/cats/dogs",
+          hash     : "",
+          search   : ""
+        },
+
+        origin : {
+          schema  : "http://www.google.com",
+          value   : "http://www.google.com",
+          isMatch : true
+        },
+
+        search : {
+          schema  : {},
+          keys    : [],
+          isMatch : true,
+        },
+
+        params : {
+          schema  : [ ":x", ":y" ],
+          value   : [ "cats", "dogs" ],
+          x       : "cats",
+          y       : "dogs",
+          isMatch : true,
+        },
+
+        hash   : {
+          schema  : "",
+          value   : "",
+          isMatch : true
+        },
+
+        isMatch : true,
+      };
+    });
 }
